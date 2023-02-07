7ce4387a369453c5bb6298be7b91d6fe489f6600
ivanpc313 <glybovskij313@mail.ru>
2023-02-07 15:44:40 +0300

    fix: fixed error of merging

--- src/ru/clevertec/FirstClassInDevelop.java
+++ src/ru/clevertec/FirstClassInDevelop.java
@@ -2,11 +2,16 @@ package ru.clevertec;
 
 public class FirstClassInDevelop {
     public static void main(String[] args) {
+
         System.out.println("New commit!");
 
         System.out.println("Fixed bugs 1");
         System.out.println("Fixed bugs 2");
         System.out.println("Fixed bugs 3");
         System.out.println("Fixed bugs 4");
+
+        int c;
+
+        double g;
     }
 }
