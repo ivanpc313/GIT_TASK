8c12a6debb64c4c815a4076ac72e034a64688f7f
ivanpc313 <glybovskij313@mail.ru>
2023-02-07 14:44:49 +0300

    style: change text

--- src/ru/clevertec/FirstClassInDevelop.java
+++ src/ru/clevertec/FirstClassInDevelop.java
@@ -2,7 +2,7 @@ package ru.clevertec;
 
 public class FirstClassInDevelop {
     public static void main(String[] args) {
-        System.out.println("New commit!");
+        System.out.println("New commit?");
 
         int c;
 
