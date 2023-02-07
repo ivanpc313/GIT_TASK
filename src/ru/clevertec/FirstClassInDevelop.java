d5f405e1055cc08ae5200bcfac120e6629093271
ivanpc313 <glybovskij313@mail.ru>
2023-02-07 16:04:53 +0300

    fix: fixed errors of merge

--- src/ru/clevertec/FirstClassInDevelop.java
+++ src/ru/clevertec/FirstClassInDevelop.java
@@ -2,7 +2,12 @@ package ru.clevertec;
 
 public class FirstClassInDevelop {
     public static void main(String[] args) {
-        System.out.println("New commit?");
+        System.out.println("New commit!");
+
+        System.out.println("Fixed bugs 1");
+        System.out.println("Fixed bugs 2");
+        System.out.println("Fixed bugs 3");
+        System.out.println("Fixed bugs 4");
 
         int c;
 
