e9ab393d06c1eac70012f33eb603461dbbec228a
ivanpc313 <glybovskij313@mail.ru>
2023-02-07 15:25:25 +0300

    feat: fixed bugs 1

--- src/ru/clevertec/FirstClassInDevelop.java
+++ src/ru/clevertec/FirstClassInDevelop.java
@@ -3,5 +3,7 @@ package ru.clevertec;
 public class FirstClassInDevelop {
     public static void main(String[] args) {
         System.out.println("New commit!");
+
+        System.out.println("Fixed bugs 1");
     }
 }
